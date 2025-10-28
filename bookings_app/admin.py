ccccd3dc7df825c793e20e2155ad2cb52ff0d197
Gokudoma <stephan_wacker@hotmail.com>
2025-10-28 14:06:47 +0100

    filtering bookings

--- bookings_app/admin.py
+++ bookings_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Booking 
 
-admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_filter = ('confirmed',) 
+    
+admin.site.register(Booking, BookingAdmin)
