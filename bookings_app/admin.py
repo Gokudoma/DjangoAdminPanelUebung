d445befd0bcbe94d8ccc746d93d6d73de1ae670f
Gokudoma <stephan_wacker@hotmail.com>
2025-10-28 14:43:03 +0100

    read_only

--- bookings_app/admin.py
+++ bookings_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Booking 
+from .models import Booking, Participant 
 
 class BookingAdmin(admin.ModelAdmin):
     list_filter = ('confirmed',) 
-    
+    readonly_fields = ('booking_date',)
+    fields = ('event', 'participant', 'confirmed', 'booking_date')
+
 admin.site.register(Booking, BookingAdmin)
+admin.site.register(Participant)
