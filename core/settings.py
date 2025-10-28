dcecd51864e10d0ea27815570008985f96e36b50
Gokudoma <stephan_wacker@hotmail.com>
2025-10-28 13:08:59 +0100

    Registering models in the Admin Panel

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events_app.apps.EventsAppConfig',   
+    'bookings_app.apps.BookingsAppConfig',
 ]
 
 MIDDLEWARE = [
